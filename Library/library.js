cfc34970ca141530d1f794fce53033b76f4e694c
Valeentin87 <Valeentin87@list.ru>
2024-09-15 11:20:31 +0300

    реализован метод добавления новой книги

--- Library/library.js
+++ Library/library.js
@@ -1,4 +1,6 @@
-const newBooks = [{'Pushkin':'Evgeniy Onegin', 'Mihail Sholohov': 'Molodaya Gvardia'}];
+const newBooks = [{author:'Pushkin',
+work:'Evgeniy Onegin'},
+{author:'Mihail Sholohov', work: 'Molodaya Gvardia'}];
 
 class Library {
     #books = [];
@@ -8,8 +10,27 @@ class Library {
     
     showAllBooks(){
         console.log(this.#books);
+    };
+    
+    addBook(title){
+        const allAuthors = [];
+        const allWorks = [];
+        let book;
+        for (book of this.#books){
+            allAuthors.push(book.author);
+            allWorks.push(book.work);
+        }
+        if (allAuthors.indexOf(title.author) !== -1 && allWorks.indexOf(title.work) !== -1) {
+            console.log('данное произведение уже есть в каталоге библиотеки');
+            return;
+        }
+        this.#books.push(title);
+        console.log("произведение добавлено");
     }
 }
 
 const firstLibrary = new Library(newBooks);
+firstLibrary.showAllBooks();
+firstLibrary.addBook({'Mihail Sholohov':'Tihiy Don'});
+firstLibrary.addBook({'Mihail Sholohov': 'Tihiy Don'});
 firstLibrary.showAllBooks();
